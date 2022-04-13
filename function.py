e308c6088b33a294e48a31b3e053a850ec5aab47
Maya Gonzalez <maya.gonzalez.b@gmail.com>
2022-04-13 17:29:28 +0200

    Arbetsprov Steg1 Maya Gonzalez

---
+++ function.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import math as calculate
+from unicodedata import decimal
+import math as math
+import csv
+import datetime
+
+
+
+def getDateTime(dataFile):
+    print("DATETIME:" + dataFile[-14:-4])
+
+   
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-import function as myFirstFunction
+import tkinter as gui
+from unicodedata import decimal
+import math
+import function as function
+import csv 
+import os
+import datetime
+import constants
+import os
 
-msg= "Execute main.py"                                         
-print(msg)
+window = gui.Tk()
+
+#Steg 1
+
+for filename in os.listdir(os.getcwd()):
+    if 'csv' in filename:
+        with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
+            #Get files with correct format
+            if constants.filePrefix in filename:
+                dateSuffix = filename[-8:-6]
+                if int(dateSuffix) <= constants.endDate and int(dateSuffix) >= constants.startDate:
+                    print(filename)
